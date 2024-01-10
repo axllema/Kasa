255ed8f3cc86324ff8c6f8ea5ff024e5b630b3a3
axllema <axellexm@gmail.com>
2024-01-10 19:38:15 +0100

    updates in error page , error scss, footer too, and so on - problem with footer on error page, to change asap

--- Kasa/src/pages/Error.jsx
+++ Kasa/src/pages/Error.jsx
@@ -1,15 +1,22 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
+import Header from '../components/header';
+import Footer from '../components/footer';
 import '../scss/pages/_error.scss';
 
 function Error() {
     return (
-    <div>
-        <h2>404 </h2>
-        <p> Oups ! La page que vous demandez n&rsquo;existe pas.</p>
-        {/* &rsquo; because ' wouldn't work */}
-        <Link to="/"> Retourner sur la page d&rsquo;accueil</Link>
-    </div>
+        <div>
+            <Header />
+            <div className='error'>
+                <h2 className='error_title'>404</h2>
+                <p className='error_text'> Oups ! La page que vous demandez n&rsquo;existe pas.</p>
+                {/* &rsquo; because ' wouldn't work */}
+                <Link className='error_return' to="/"> Retourner sur la page d&rsquo;accueil</Link>
+            </div>
+            {/* Mettez le Footer ici à l'extérieur de la div 'error' */}
+            <Footer />
+        </div>
     );
 }
 
