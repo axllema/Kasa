d2a1c366facdec0f86bc465a37e8454801970cd3
axllema <axellexm@gmail.com>
2024-01-23 15:53:58 +0100

    loader hidden for the moment and changes in scss

--- Kasa/src/pages/Home.jsx
+++ Kasa/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import Loader from '../components/loader';
+/* import Loader from '../components/loader'; */
 import Header from '../components/header';
 import Banner from '../components/banner';
 import Gallery from '../components/gallery';
@@ -8,7 +8,6 @@ import Footer from '../components/footer';
 function Home() {
     return (
         <div>
-            <Loader /> 
             <Header /> 
             <Banner showSecond={false} showFirst={true} /> 
             {/* renders Banner component with showSecond set to false and showFirst set to true */}
