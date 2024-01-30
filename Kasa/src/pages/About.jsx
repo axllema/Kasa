28ab1c28f64be31f7092a481e16e203cd081dc54
axllema <axellexm@gmail.com>
2024-01-31 00:19:35 +0100

    changes in about scss & about jsx - changes 'p' for 'btn_container__description' bc it was causing problem elsewhere

--- Kasa/src/pages/About.jsx
+++ Kasa/src/pages/About.jsx
@@ -14,26 +14,26 @@ function About() {
 
             <div className='first_container'>
                 <CollapseSection title="Fiabilité" containerClassName="btn_container" buttonClassName="btn-about">
-                    <p> Les annonces postées sur Kasa garantissent une fiabilité totale.
+                    <p className='btn_container__description'> Les annonces postées sur Kasa garantissent une fiabilité totale.
                         Les photos sont conformes aux logements, et toutes les informations
                         sont régulièrement vérifiées par nos équipes.
                     </p>
                 </CollapseSection>
 
                 <CollapseSection title="Respect" containerClassName="btn_container" buttonClassName="btn-about">
-                    <p> Le respect fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire
+                <p className='btn_container__description'> Le respect fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire
                         ou de perturbation du voisinage entraînera une exclusion de notre plateforme.
                     </p>
                 </CollapseSection>
 
                 <CollapseSection title="Service" containerClassName="btn_container" buttonClassName="btn-about">
-                    <p> La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement
+                <p className='btn_container__description'> La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement
                         discriminatoire ou de perturbation du voisinage entraînera une exclusion de notreplateforme.
                         </p>
                 </CollapseSection>
 
                 <CollapseSection title="Sécurité" containerClassName="btn_container" buttonClassName="btn-about">
-                    <p> La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs,
+                <p className='btn_container__description'> La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs,
                         chaque logement correspond aux critères de sécurité établis par nos services.
                         En laissant une note aussi bien à l&rsquo;hôte qu&rsquo;au locataire, cela permet à
                         nos équipes de vérifier que les standards sont bien respectés. Nous organisons
