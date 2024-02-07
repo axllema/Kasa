230130d2449d543b8a0cb9bb919f5ac0a695a465
axllema <axellexm@gmail.com>
2024-02-07 16:06:12 +0100

    carrousel counter + arrows only if more than 1 pic

--- Kasa/src/components/carrousel.jsx
+++ Kasa/src/components/carrousel.jsx
@@ -23,33 +23,46 @@ const nextPicture = () => {
         );
     };
 
+    // determines if there's only one picture
+    const isSinglePicture = pictures.length === 1;
+
     // renders the Carrousel component with buttons to navigate between pictures
     return (
         <div className="carrousel">
             {pictures.length > 0 && (
                 <>
-                <div className="carrousel__img-container">
-                    <img
-                        className="carrousel__img"
-                        src={pictures[currentPictureIndex]}
-                        alt={`Slide ${currentPictureIndex + 1}`}
-                    />
-                    <div className="carrousel__counter">
-                        {currentPictureIndex + 1}/{pictures.length}
+                    <div className="carrousel__img-container">
+                        <img
+                            className="carrousel__img"
+                            src={pictures[currentPictureIndex]}
+                            alt={`Slide ${currentPictureIndex + 1}`}
+                        />
+                        
+                        {/* renders the counter only if there's more than one picture */}
+                        {!isSinglePicture && (
+                            <div className="carrousel__counter">
+                                {currentPictureIndex + 1}/{pictures.length}
+                            </div>
+                        )}
                     </div>
-                </div>
                 </>
             )}
+
             {/* In React, the empty angle brackets <> and </> are used to create a fragment.
             A fragment is a way to group multiple children elements without adding extra nodes to the DOM. It's a shorthand syntax for <React.Fragment>.
             In the provided code, the fragment <>...</> is used to wrap multiple elements inside the {pictures.length > 0 && (...)} conditional block. */}
 
-            <button className="carrousel__left_arrow" onClick={prevPicture}>
-                &lt;
-            </button>
-            <button className="carrousel__right_arrow" onClick={nextPicture}>
-                &gt;
-            </button>
+             {/* renders arrows only if there's more than one picture */}
+            {pictures.length > 1 && (
+                <>
+                    <button className="carrousel__left_arrow" onClick={prevPicture}>
+                        &lt;
+                    </button>
+                    <button className="carrousel__right_arrow" onClick={nextPicture}>
+                        &gt;
+                    </button>
+                </>
+            )}
         </div>
     );
     };
