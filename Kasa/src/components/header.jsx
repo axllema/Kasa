a8795f77919df2186d25e167dac2f00b012a003e
axllema <axellexm@gmail.com>
2024-02-12 18:54:30 +0100

    arrows animation smooth & rorating to the left + active link/a in nav when cliked/onpage

--- Kasa/src/components/header.jsx
+++ Kasa/src/components/header.jsx
@@ -8,11 +8,14 @@ function Header () {
     return <div className="header">
         <Logo />
             <nav>
-                <Link to="/">Accueil</Link>
-                <Link to="/about">A propos</Link>
+                <Link to="/" className={location.pathname === '/' ? 'active' : ''}> Accueil </Link>
+                <Link to="/about" className={location.pathname === '/about' ? 'active' : ''}> A propos </Link>
             </nav>
     </div>
-    
 }
 
-export default Header
+{/*  The <Link> tags create navigation links in the application using React Router - each link points to a specific URL defined by the "to" attribute.
+For each link in the navigation menu, we check if the current page's URL matches the link's URL. If it does, we add the 'active' class to the link,
+which highlights it to indicate to the user that they are on that page. (here : the "Accueil" ou "À propos" is underlined) */}
+
+export default Header;
