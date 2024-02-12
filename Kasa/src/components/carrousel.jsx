408173d62f939627da7ff0046ae4042206ac928b
axllema <axellexm@gmail.com>
2024-02-12 16:11:34 +0100

    delete com

--- Kasa/src/components/carrousel.jsx
+++ Kasa/src/components/carrousel.jsx
@@ -2,21 +2,13 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import '../scss/components/_carrousel.scss';
 
-// defines the Carrousel component
 const Carrousel = ({ pictures }) => {
-    // states to keep track of the current picture index
     const [currentPictureIndex, setCurrentPictureIndex] = useState(0);
 
-   // function to go to the next picture
 const nextPicture = () => {
-    // setCurrentPictureIndex is a function from the useState hook to update the state
     setCurrentPictureIndex((prevIndex) => (prevIndex + 1) % pictures.length); 
-    {/* the function takes the previous index (prevIndex) and increments it by 1 -
-    if the result is equal to pictures.length, it wraps around to 0 using the modulus operator (%).
-    this way, it creates a looping effect, allowing the carousel to cycle through pictures. */}
 };
 
-    // function to go to the previous picture
     const prevPicture = () => {
         setCurrentPictureIndex(
             (prevIndex) => (prevIndex - 1 + pictures.length) % pictures.length
@@ -48,9 +40,6 @@ const nextPicture = () => {
                 </>
             )}
 
-            {/* In React, the empty angle brackets <> and </> are used to create a fragment.
-            A fragment is a way to group multiple children elements without adding extra nodes to the DOM. It's a shorthand syntax for <React.Fragment>.
-            In the provided code, the fragment <>...</> is used to wrap multiple elements inside the {pictures.length > 0 && (...)} conditional block. */}
 
              {/* renders arrows only if there's more than one picture */}
             {pictures.length > 1 && (
@@ -67,7 +56,6 @@ const nextPicture = () => {
     );
     };
 
-// PropTypes for the Carrousel component
 Carrousel.propTypes = {
     pictures: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
